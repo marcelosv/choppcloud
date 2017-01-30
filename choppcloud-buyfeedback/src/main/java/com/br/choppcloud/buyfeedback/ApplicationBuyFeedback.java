54265ae6d19562940c8ae7be1df349e8561fede1
Marcelo Souza Vieira <marcelo.vieira@involves.com.br>
2017-01-30 19:01:05 -0200

    alter

--- choppcloud-buyfeedback/src/main/java/com/br/choppcloud/buyfeedback/ApplicationBuyFeedback.java
+++ choppcloud-buyfeedback/src/main/java/com/br/choppcloud/buyfeedback/ApplicationBuyFeedback.java
@@ -2,10 +2,12 @@ package com.br.choppcloud.buyfeedback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableRedisRepositories
+@EnableDiscoveryClient
 public class ApplicationBuyFeedback {
 
 	public static void main(String[] args) throws InterruptedException {
--- choppcloud-buytrip/src/main/java/com/br/choppcloud/buytrip/ApplicationBuyTrip.java
+++ choppcloud-buytrip/src/main/java/com/br/choppcloud/buytrip/ApplicationBuyTrip.java
@@ -2,8 +2,10 @@ package com.br.choppcloud.buytrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApplicationBuyTrip {
 
     public static void main(String[] args) {
