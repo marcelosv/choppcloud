4b7e8e89fa14fb654012c37a21d18972e2d16754
Marcelo Souza Vieira <marcelo.vieira@involves.com.br>
2017-01-31 13:17:58 -0200

    alter

--- choppcloud-buyprocess/src/main/java/com/br/choppcloud/buyprocess/ApplicationBuyProcess.java
+++ choppcloud-buyprocess/src/main/java/com/br/choppcloud/buyprocess/ApplicationBuyProcess.java
@@ -2,8 +2,10 @@ package com.br.choppcloud.buyprocess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class ApplicationBuyProcess {
 
 	public static void main(String[] args) throws InterruptedException {
