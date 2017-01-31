61d191021908a1e5bcf4d01a611c4235bee04d21
Marcelo Souza Vieira <marcelo.vieira@involves.com.br>
2017-01-31 11:02:59 -0200

    alter

---
+++ passos/infra/infra-4-CompraController.java
@@ -0,0 +1,31 @@
+package com.br.choppcloud.buyfeedback.finalizar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CompraController {
+
+	@Autowired
+	private CompraRedisRepository compraRedisRepository;
+	
+	@RequestMapping(path = "/{chave}", method = RequestMethod.GET)
+	public CompraRedis status(@PathVariable("chave") String chave){
+		
+		CompraRedis compra = compraRedisRepository.findOne(chave);
+		
+		if( compra == null ){
+			throw new NaoFinalizadoException();
+		}
+		
+		return compra;
+	}
+	
+	@RequestMapping(path = "/meunome", method = RequestMethod.GET)
+	public String status(){
+		return "Estou na máquina do: Marcelo";
+	}
+}
